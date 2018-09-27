51a9aea7c332ba22f3df05101d620f86746ae5ee
yuan <ghostkingston@gmail.com>
2018-09-27 22:48:28 +0800

    做成百分比对比

--- testbook/ch1-1.py
+++ testbook/ch1-1.py
@@ -12,12 +12,12 @@ import matplotlib.pyplot as plt
 
 myfont = mpl.font_manager.FontProperties(
     fname='/Users/yuan/pycode/font/simhei.ttf')
-plt.plot([-2, 2, 3, 4, 5], 'r', label='第一条曲线')
-plt.plot([3, 4, 5, 8, 9], 'b', label='第二条曲线')
+plt.plot([0.727, 0.641, 0.678, 0.714, 0.628], 'r', label='20180927')
+plt.plot([0, 0, 0, 0, 0], 'b', label='第二条曲线')
 plt.legend()
 plt.grid(True)
-plt.axis([0, 5, -3, 9])
+plt.axis([0, 5, 0, 1])
 plt.xlabel('X-轴坐标', FontProperties=myfont)
 plt.ylabel('Y-轴坐标', FontProperties=myfont)
-plt.title('matplotlib 演示图', FontProperties=myfont)
+plt.title('百分比演化图', FontProperties=myfont)
 plt.show()
