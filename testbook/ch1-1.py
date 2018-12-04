a22d0470c5a995baf40d2939845e25cfcdc4eb38
yuan <ghostkingston@gmail.com>
2018-12-04 20:06:17 +0800

    vscode test

--- testbook/ch1-1.py
+++ testbook/ch1-1.py
@@ -12,11 +12,12 @@ import matplotlib.pyplot as plt
 
 myfont = mpl.font_manager.FontProperties(
     fname='/Users/yuan/pycode/font/simhei.ttf')
-plt.plot([0.727, 0.641, 0.678, 0.714, 0.628], 'r', label='20180927')
+plt.plot([727, 641, 678, 714, 628], 'r', label='20180927')
+plt.plot([725, 636, 678, 714, 627], 'g', label='20180928')
 plt.plot([0, 0, 0, 0, 0], 'b', label='第二条曲线')
 plt.legend()
 plt.grid(True)
-plt.axis([0, 5, 0, 1])
+plt.axis([0, 5, 0, 1000])
 plt.xlabel('X-轴坐标', FontProperties=myfont)
 plt.ylabel('Y-轴坐标', FontProperties=myfont)
 plt.title('百分比演化图', FontProperties=myfont)
