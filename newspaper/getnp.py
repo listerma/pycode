9f43bf466401f76cb8dff2bebbcd5349f0045efd
yuan <ghostkingston@gmail.com>
2019-01-13 12:07:48 +0800

    vscode-setting

--- newspaper/getnp.py
+++ newspaper/getnp.py
@@ -1,5 +1,8 @@
-from datetime import datetime
+from datetime import date
 
 
-now = datetime.now()
-print(now)
+#获取当前日期，格式化为日报地址模式year-month/day
+tdate = date.today()
+print(tdate)
+datetmp = str(tdate).split("-",1)
+print(datetmp)
